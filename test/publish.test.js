579424741197465c365d0793c76b22cbe0488aea
nmccready <nemtcan@gmail.com>
2017-04-24 21:18:39 -0400

    bump 0.0.3 fixed publishing

---
+++ test/publish.test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var exec = require('child_process').exec;
+
+function cleanUp(cb) {
+  return exec('rm -rf ./tmp', cb);
+}
+
+function makeTestPackage(cb) {
+  return exec('./scripts/mockPublish', cb);
+}
+
+describe('publish: can load a published version', function() {
+  it('publish: can load a published version', function(done) {
+    // return t.fail("mock fail");
+    cleanUp(function() {
+      makeTestPackage(function() {
+        try {
+          // attempt to load a packed / unpacked potential deployed version
+          require('../tmp/package/dist');
+        }
+        catch (error){
+          throw error;
+        }
+        finally{
+          cleanUp(function() {
+            done()
+          });
+        }
+      });
+    });
+  });
+});
